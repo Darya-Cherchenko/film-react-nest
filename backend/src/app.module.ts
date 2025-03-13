703ef2e6548b24f958a440faed49d79e7ca36c34
DaryaCherchenko <cherchenkodarya@gmail.com>
2025-03-13 20:00:44 +0300

    на ревью 1 (16)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,31 @@
 import { Module } from '@nestjs/common';
-import {ServeStaticModule} from "@nestjs/serve-static";
-import {ConfigModule} from "@nestjs/config";
-import * as path from "node:path";
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
+import * as path from 'node:path';
+import { MongooseModule } from '@nestjs/mongoose';
 
-import {configProvider} from "./app.config.provider";
+import { applicationConfig, configProvider } from './app.config.provider';
+import { FilmsController } from './films/films.controller';
+import { OrderController } from './order/order.controller';
+import { FilmsService } from './films/films.service';
+import { OrderService } from './order/order.service';
+import { Film, FilmSchema } from './films/films.schema';
+import { FilmsRepository } from './repository/films.repository';
 
 @Module({
   imports: [
-	ConfigModule.forRoot({
-          isGlobal: true,
-          cache: true
-      }),
-      // @todo: Добавьте раздачу статических файлов из public
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+    }),
+    MongooseModule.forRoot(applicationConfig.DATABASE_URL),
+    MongooseModule.forFeature([{ name: Film.name, schema: FilmSchema }]),
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '..', 'public'),
+      renderPath: '/content/afisha/',
+    }),
   ],
-  controllers: [],
-  providers: [configProvider],
+  controllers: [FilmsController, OrderController],
+  providers: [configProvider, FilmsService, OrderService, FilmsRepository],
 })
 export class AppModule {}
--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -10,5 +10,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'quotes': [2, 'single', { 'avoidEscape': true }],
   },
 }
